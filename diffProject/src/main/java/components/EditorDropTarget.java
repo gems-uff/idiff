7481938f5f4230881aeefc3f3e78869b278446b3
fernanda <fernanda@fccd8bbc-fb64-447e-9817-562b7b49459e>
2011-06-22 04:36:53 +0000

    Alterações no dnd

--- diffProject/src/main/java/components/EditorDropTarget.java
+++ diffProject/src/main/java/components/EditorDropTarget.java
@@ -28,6 +28,11 @@ import javax.swing.JTree;
  */
 public class EditorDropTarget implements DropTargetListener, PropertyChangeListener {
 
+    /**
+     * Constructor
+     * @param pane
+     * @param tree 
+     */
     EditorDropTarget(JEditorPane pane, JTree tree) {
         this.pane = pane;
         this.tree = tree;
@@ -35,68 +40,66 @@ public class EditorDropTarget implements DropTargetListener, PropertyChangeListe
         dropTarget = new DropTarget(pane, DnDConstants.ACTION_COPY_OR_MOVE, this, pane.isEnabled(), null);
     }
 
+    /**
+     * Drag Enter
+     * @param dtde 
+     */
     @Override
     public void dragEnter(DropTargetDragEvent dtde) {
         DnDUtils.debugPrintln("dragEnter, drop action = "
                 + DnDUtils.showActions(dtde.getDropAction()));
-        System.out.println("1");
-        // Get the type of object being transferred and determine
-        // whether it is appropriate.
         checkTransferType(dtde);
-
-        // Accept or reject the drag.
         boolean acceptedDrag = acceptOrRejectDrag(dtde);
-
-        // Do drag-under feedback
         dragUnderFeedback(dtde, acceptedDrag);
     }
 
+    /**
+     * drag Exit
+     * @param dte 
+     */
     @Override
     public void dragExit(DropTargetEvent dte) {
         DnDUtils.debugPrintln("DropTarget dragExit");
-        System.out.println("2");
-        // Do drag-under feedback
         dragUnderFeedback(null, false);
     }
 
+    /**
+     * drag Over
+     * @param dtde 
+     */
     @Override
     public void dragOver(DropTargetDragEvent dtde) {
         DnDUtils.debugPrintln("DropTarget dragOver, drop action = "
                 + DnDUtils.showActions(dtde.getDropAction()));
-        System.out.println("3");
-        // Accept or reject the drag
         boolean acceptedDrag = acceptOrRejectDrag(dtde);
-
-        // Do drag-under feedback
         dragUnderFeedback(dtde, acceptedDrag);
     }
 
+    /**
+     * Drop Action Changed
+     * @param dtde 
+     */
     @Override
     public void dropActionChanged(DropTargetDragEvent dtde) {
         DnDUtils.debugPrintln("DropTarget dropActionChanged, drop action = "
                 + DnDUtils.showActions(dtde.getDropAction()));
-        System.out.println("4");
-        // Accept or reject the drag
         boolean acceptedDrag = acceptOrRejectDrag(dtde);
-
-        // Do drag-under feedback
         dragUnderFeedback(dtde, acceptedDrag);
     }
 
+    /**
+     * Drop
+     * @param dtde 
+     */
     @Override
     public void drop(DropTargetDropEvent dtde) {
         DnDUtils.debugPrintln("DropTarget drop, drop action = "
                 + DnDUtils.showActions(dtde.getDropAction()));
-        System.out.println("5");
-        // Check the drop action
         if ((dtde.getDropAction() & DnDConstants.ACTION_COPY_OR_MOVE) != 0) {
-            // Accept the drop and get the transfer data
             dtde.acceptDrop(dtde.getDropAction());
             Transferable transferable = dtde.getTransferable();
-
             try {
                 boolean result = false;
-
                 if (draggingFile) {
                     result = dropFile(transferable);
                 } else {
@@ -115,48 +118,42 @@ public class EditorDropTarget implements DropTargetListener, PropertyChangeListe
         }
     }
 
-    // PropertyChangeListener interface
+    /**
+     * Property Change
+     * @param evt 
+     */
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         String propertyName = evt.getPropertyName();
-        System.out.println("6");
         if (propertyName.equals("enabled")) {
-            // Enable the drop target if the JEditorPane is enabled
-            // and vice versa.
             dropTarget.setActive(pane.isEnabled());
-        }// else if (!changingBackground && propertyName.equals("background")) {
-        //  backgroundColor = pane.getBackground();
-        //}
+        }
     }
 
-    // Internal methods start here
+    /**
+     * Accept Or Reject Drag
+     * @param dtde
+     * @return boolean
+     */
     protected boolean acceptOrRejectDrag(DropTargetDragEvent dtde) {
         int dropAction = dtde.getDropAction();
         int sourceActions = dtde.getSourceActions();
         boolean acceptedDrag = false;
-        System.out.println("7");
         DnDUtils.debugPrintln("\tSource actions are "
                 + DnDUtils.showActions(sourceActions) + ", drop action is "
                 + DnDUtils.showActions(dropAction));
-
-        // Reject if the object being transferred
-        // or the operations available are not acceptable.
         if (!acceptableType
                 || (sourceActions & DnDConstants.ACTION_COPY_OR_MOVE) == 0) {
             DnDUtils.debugPrintln("Drop target rejecting drag");
             dtde.rejectDrag();
         } else if (!draggingFile && !pane.isEditable()) {
-            // Can't drag text to a read-only JEditorPane
             DnDUtils.debugPrintln("Drop target rejecting drag");
             dtde.rejectDrag();
         } else if ((dropAction & DnDConstants.ACTION_COPY_OR_MOVE) == 0) {
-            // Not offering copy or move - suggest a copy
-            //TODO VER AQUI
             DnDUtils.debugPrintln("Drop target offering COPY");
             dtde.acceptDrag(DnDConstants.ACTION_COPY);
             acceptedDrag = true;
         } else {
-            // Offering an acceptable operation: accept
             DnDUtils.debugPrintln("Drop target accepting drag");
             dtde.acceptDrag(dropAction);
             acceptedDrag = true;
@@ -165,22 +162,14 @@ public class EditorDropTarget implements DropTargetListener, PropertyChangeListe
         return acceptedDrag;
     }
 
+    /**
+     * Drag Under Feedback
+     * @param dtde
+     * @param acceptedDrag 
+     */
     protected void dragUnderFeedback(DropTargetDragEvent dtde,
             boolean acceptedDrag) {
-        System.out.println("8");
-        // if (draggingFile) {
-        // When dragging a file, change the background color
-        //   Color newColor = (dtde != null && acceptedDrag ? feedbackColor
-        //         : backgroundColor);
-        // if (newColor.equals(pane.getBackground()) == false) {
-        // changingBackground = true;
-        //   pane.setBackground(newColor);
-        //  changingBackground = false;
-        // pane.repaint();
-        //}
-        //} else {
         if (dtde != null && acceptedDrag) {
-            // Dragging text - move the insertion cursor
             Point location = dtde.getLocation();
             pane.getCaret().setVisible(true);
             pane.setCaretPosition(pane.viewToModel(location));
@@ -189,10 +178,11 @@ public class EditorDropTarget implements DropTargetListener, PropertyChangeListe
         }
     }
 
+    /**
+     * Check Transfer Type
+     * @param dtde 
+     */
     protected void checkTransferType(DropTargetDragEvent dtde) {
-        // Accept a list of files, or data content that
-        // amounts to plain text or a Unicode text string
-        System.out.println("9");
         acceptableType = false;
         draggingFile = false;
 
@@ -206,32 +196,38 @@ public class EditorDropTarget implements DropTargetListener, PropertyChangeListe
         DnDUtils.debugPrintln("Dragging a file - " + draggingFile);
     }
 
-    // This method handles a drop for a list of files
+    /**
+     * Drop File
+     * @param transferable
+     * @return boolean
+     * @throws IOException
+     * @throws UnsupportedFlavorException
+     * @throws MalformedURLException 
+     */
     protected boolean dropFile(Transferable transferable) throws IOException,
             UnsupportedFlavorException, MalformedURLException {
         List fileList = (List) transferable.getTransferData(DataFlavor.javaFileListFlavor);
         File transferFile = (File) fileList.get(0);
-        System.out.println("10");
         final URL transferURL = transferFile.toURI().toURL();
         DnDUtils.debugPrintln("File URL is " + transferURL);
         pane.setPage(transferURL);
         return true;
     }
 
-    // This method handles a drop with data content
+    /**
+     * Drop Content
+     * @param transferable
+     * @param dtde
+     * @return boolean
+     */
     protected boolean dropContent(Transferable transferable,
             DropTargetDropEvent dtde) {
-        //TODO IMPLEMENTAR AQUI
-        System.out.println("11");
         if (!pane.isEditable()) {
-            // Can't drop content on a read-only text control
             return false;
         }
         try {
-            // Check for a match with the current content type
             DataFlavor[] flavors = dtde.getCurrentDataFlavors();
             DataFlavor selectedFlavor = null;
-            // Look for either plain text or a String.
             for (int i = 0; i < flavors.length; i++) {
                 DataFlavor flavor = flavors[i];
                 DnDUtils.debugPrintln("Drop MIME type " + flavor.getMimeType()
@@ -243,13 +239,11 @@ public class EditorDropTarget implements DropTargetListener, PropertyChangeListe
             }
 
             if (selectedFlavor == null) {
-                // No compatible flavor - should never happen
                 return false;
             }
 
             DnDUtils.debugPrintln("Selected flavor is "
                     + selectedFlavor.getHumanPresentableName());
-
             // Get the transferable and then obtain the data
             Object data = transferable.getTransferData(selectedFlavor);
 
@@ -258,7 +252,6 @@ public class EditorDropTarget implements DropTargetListener, PropertyChangeListe
 
             String insertData = null;
             if (data instanceof InputStream) {
-                // Plain text flavor
                 String charSet = selectedFlavor.getParameter("charset");
                 InputStream is = (InputStream) data;
                 byte[] bytes = new byte[is.available()];
@@ -266,11 +259,9 @@ public class EditorDropTarget implements DropTargetListener, PropertyChangeListe
                 try {
                     insertData = new String(bytes, charSet);
                 } catch (UnsupportedEncodingException e) {
-                    // Use the platform default encoding
                     insertData = new String(bytes);
                 }
             } else if (data instanceof String) {
-                // String flavor
                 insertData = (String) data;
             }
 
@@ -289,9 +280,6 @@ public class EditorDropTarget implements DropTargetListener, PropertyChangeListe
     protected JEditorPane pane;
     protected JTree tree;
     protected DropTarget dropTarget;
-    protected boolean acceptableType; // Indicates whether data is acceptable
-    protected boolean draggingFile; // True if dragging an entire file
-    //protected Color backgroundColor; // Original background color of JEditorPane
-    //protected boolean changingBackground;
-    //   protected static final Color feedbackColor = Color.gray;
+    protected boolean acceptableType; 
+    protected boolean draggingFile; 
 }
