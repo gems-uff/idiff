fe9226b631c77324d234607a5f2e7b883c5fb214
fernanda <fernanda@fccd8bbc-fb64-447e-9817-562b7b49459e>
2012-06-04 11:40:03 +0000

    Introdução de Contantes

---
+++ src/main/java/idiff/resources/Constants.java
@@ -0,0 +1,29 @@
+package idiff.resources;
+
+/**
+ *
+ * @author Fernanda Floriano Silva
+ */
+public class Constants {
+
+    public static final String TAB = "§";
+    public static final String SPACE = " ";
+
+    public static final int ADD_STYLE = 8;
+    public static final int DISABLE_STYLE = 13;
+    public static final int MOVE_STYLE = 9;
+    public static final int REMOVE_STYLE = 11;
+    public static final int UNCHANGED_STYLE = 14;
+    
+    public static final int LEFT_DIRECTORY = 1;
+    public static final int RIGHT_DIRECTORY = 2;
+    
+    public static final int SIMILARITY_AVG = 50;
+    
+    public static final int DIFF_PERSPECTIVE = 1;
+    public static final int SIMILARITY_PERSPECTIVE = 2;
+    
+    public static final String LINE_BREAK_ATTRIBUTE_NAME = "line_break_attribute";
+    
+    public static final int PERCENTUAL_IDENTICO = 100;
+}
